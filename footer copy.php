0ce10e1eb3494dc88401dcc3b0177a9a67ceaeba
Nielan2008 <nap.ukdh@gmail.com>
2025-09-11 14:23:36 +0200

    footer+css

---
+++ footer copy.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+
+
+<div class="footer">
+  <div class="footer_container_1">
+  <img class="footer_logo_img" src="img/logo_hoofd.png" alt="logo">
+  <p class="footer_text_1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,</p>
+  <button class="footer_button">Lees Meer</button>
+  </div>
+  <div class="footer_container_2">
+  <p class="footer_text_2_big">NAVIGEER</p>
+  <p class="footer_text_2">
+  <a href="">Werken bij</a><br>
+   <a href="">Veelgestelde vragen</a><br>
+   <a href="">Vestigingen</a><br>
+   <a href="">Contact</a><br>
+  </p>
+</div>
+<div class="footer_container_3">
+  <p class="footer_text_3">VOLG ONS</p>
+  <div class="footer_socials">
+    <img class="footer_socials_img" src="img/facebook.png" alt="facebook">
+    <img class="footer_socials_img" src="img/twitter.png" alt="twitter">
+    <img class="footer_socials_img" src="img/instagram.png" alt="instagram">
+</div>
+</div>
+<img class="footer_bg_img" src="img/movie_ding.png" alt="footer_bg">
+</div>
+<div class="footer_bar">Voorwaarden | Privacy beleid | Cookie disclaimer</div>
+
+<!-- a -->
+
+</body>
+</html>

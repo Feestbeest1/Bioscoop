414072c146fdb4e05643f0f4ad0f3cd3d6330cfa
Feestbeest1 <d.mateman18@gmail.com>
2025-09-11 10:08:42 +0200

    Push

---
+++ header.php
@@ -0,0 +1,38 @@
+<header>
+    <div class="logo">
+        <img src="img/logo.jpeg" alt="logo">
+    <div class="logonaam">
+        <p>AnnexBios</p>
+        <img src="img/filmrol.jpeg" alt="filmrol">
+    </div>
+    <div class="streep">
+        <p>|</p>
+    </div>
+    <div class="bioscoop-naam">
+        <p>Hellevoetsluis</p>
+    </div>
+    </div>
+
+    <div class="button-container">
+        <button>FILM AGENDA</button>
+        <button>ALLE VESTIGINGEN</button>
+        <button>CONTACT</button>
+    </div> 
+
+
+    <div class=stroke>
+            <ul>
+            <li><a href="#" class="text-header">KOOP JE TICKETS</a></li>
+            <li></li>
+            <select class="films">
+                <option class="kies-je-film" selected hidden>Kies je film</option>
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+            </select>
+            <li><a href="#" class="bestel-tickets">BESTEL TICKETS</a></li>
+        </ul>
+    </div>
+  <br><br>
+</header>

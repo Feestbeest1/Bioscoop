1cda3303e442f75be38186e94d889d8e2f41fba9
Feestbeest1 <d.mateman18@gmail.com>
2025-09-11 13:48:48 +0200

    product detail pagina

---
+++ filmdetailpagina.php
@@ -0,0 +1,24 @@
+<?php
+include("header.php");
+?>
+<link rel="stylesheet" href="style.css">
+
+
+<div class=film-info>
+    <div class="Titel-film">
+        <p>Titel van de film</p>
+    </div>
+    <div class="content-wrapper">
+        <div class="afbeelding-film"></div>
+        <div class="information">
+            <div class="reviews">
+                <p>Reviews</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="buy-button">
+        <button>KOOP JE TICKETS</button>
+    </div>
+    <div class="trailer"></div>
+</div>

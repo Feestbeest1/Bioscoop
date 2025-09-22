a1c1bc07c33f4224142949c18a36418f2efd323d
Nielan2008 <nap.ukdh@gmail.com>
2025-09-22 09:09:24 +0200

    klaar

--- bestelpagina.php
+++ bestelpagina.php
@@ -5,6 +5,63 @@
 
 <body>
     <div id="kopbalkbestel">TICKETS BESTELLEN</div>
+
+    <div class="bestelpagina">
+    <p class="form-text">STAP 4: VUL JE GEGEVENS IN</p>
+
+    <div id="form-input">
+      <form action="bestelpagina.php" method="post">
+
+        <div class="form-row">
+          <input type="text" name="naam" placeholder="Achternaam" required class="form-half">
+          <input type="text" name="voor_naam" placeholder="Voornaam" required class="form-half">
+        </div>
+
+        <div class="form-row">
+          <input type="email" name="email" placeholder="E-mail" required class="form-full">
+        </div>
+
+        <div class="form-row">
+          <input type="email" name="telefoon" placeholder="Telefoon" required class="form-full">
+        </div>
+
+        <p class="form-text">STAP 5: KIES JE BETAALWIJZE</p>
+
+        <!-- payment options -->
+        <div class="card-select-row">
+          <label class="card-option">
+            <input type="radio" name="card_type" value="visa" required>
+            <img src="img/nationale_bioscoop_bon.png" alt="Visa" class="card-img">
+          </label>
+          <label class="card-option">
+            <input type="radio" name="card_type" value="mastercard">
+            <img src="img/maestro.png" alt="Mastercard" class="card-img">
+          </label>
+          <label class="card-option">
+            <input type="radio" name="card_type" value="amex">
+            <img src="img/ideal.png" alt="Amex" class="card-img">
+          </label>
+        </div>
+        <!-- end payment options -->
+
+        <!-- voorwaarden block now separate -->
+        <div class="voorwaarden-container">
+          <div class="form-row checkbox-row" style="align-items:center;">
+            <input type="checkbox" id="voorwaarden" name="voorwaarden" required style="margin-right:8px;">
+            <label for="voorwaarden" style="margin:0;">
+              Ik accepteer de <a href="#" target="_blank">algemene voorwaarden</a>
+            </label>
+          </div>
+
+          <div class="form-row">
+            <input type="submit" value="Bestel">
+          </div>
+        </div>
+
+      </form>
+    </div>
+  </div>
+
 </body>
 
 <?php 

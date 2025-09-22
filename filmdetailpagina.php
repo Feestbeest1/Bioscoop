222e2249c06170c611c386fcaca94f28c20960da
Feestbeest1 <d.mateman18@gmail.com>
2025-09-22 08:40:47 +0200

    reviews

--- filmdetailpagina.php
+++ filmdetailpagina.php
@@ -26,6 +26,11 @@ curl_close($ch);
 
 $filledStars = round($movieData["movie"]["vote_average"] / 2);
 $stars = 5 - $filledStars;
+$totalStars = 5;
+
+echo $filledStars;
+echo " ";
+echo $stars;
 
 include("includes/header.php");
 include("includes/topbar.php");
@@ -40,28 +45,21 @@ include("includes/topbar.php");
     <div class="content-wrapper">
         <div class="afbeelding-film"></div>
         <div class="information">
-            <!-- <img src="img/ster.svg" alt="Film Poster" class="film-poster">
-            <img src="img/ster_open.svg" alt="Film Poster" class="film-poster"> -->
             <div class="stars-container">
-                <?php for ($i = 0; $i < $filledStars; $i++): ?>
-                    <div class="star filled">
-                        <svg viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg">
-                            <polygon class="star-shape" points="974.7,524.7 1007.4,530.4 983,552.9 987.6,585.8 958.7,569.5 928.9,584.1 935.4,551.5 912.3,527.7 
-                            945.3,523.9 960.8,494.5"/>
-                        </svg>
+                <?php for ($i = 0; $i < $totalStars; $i++): ?>
+                    <?php if ($i < $filledStars): ?>
+                        <div class="star-filled">
+                            <img src="sterretje.svg" alt="Filled Star">
                     </div>
-                <?php endfor; ?>
-
-                <?php for ($i = 0; $i < $stars; $i++): ?>
-                    <div class="star">
+                    <?php else: ?>
+                        <div class="star notfilled">
                         <svg viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg">
                             <polygon class="star-shape" points="1101.7,524.7 1134.4,530.4 1110,552.9 1114.6,585.8 1085.7,569.5 1055.9,584.1 1062.4,551.5 
                             1039.3,527.7 1072.3,523.9 1087.8,494.5"/>
                         </svg>
                     </div>
+                    <?php endif; ?>
                 <?php endfor; ?>
-
-
             </div>
             <?php echo $movieData["movie"]["vote_average"]; ?> 
             <div class="status"><?php echo $movieData["movie"]["status"]; ?></div>

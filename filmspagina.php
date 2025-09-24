75d091fc5d5af476c03dff7243a6975e535edc79
SheneB07 <shenebanga@gmail.com>
2025-09-24 10:46:10 +0200

    filmagenda aanpassingen

--- filmspagina.php
+++ filmspagina.php
@@ -54,14 +54,12 @@
         <!-- <input type="radio" name="film-filter" id="categorie"> -->
         <select class="categorie-box">
             <option>CATEGORIE</option>
-            <option>ACTIE</option>
-            <option>AVONTUUR</option>
-            <option>KOMEDIE</option>
-            <option>FANTASIE</option>
-            <option>DRAMA</option>
-            <option>HORROR</option>
-            <option>ROMANTIEK</option>
-            <option>THRILLER</option>
+            <option>ACTION</option>
+            <option>ADVENTURE</option>
+            <option>ANIMATION</option>
+            <option>COMEDY</option>
+            <option>CRIME</option>
+            <option>DOCUMENTARY</option>
         </select>
 </label>
     </div>
@@ -74,12 +72,77 @@
     <div id="filmsContainer"></div>
 
     <script>
-        const container = document.getElementById("filmsContainer");
+       const container = document.getElementById("filmsContainer");
         for (let i = 0; i < 24; i++) {
             const filmBox = document.createElement("div");
             filmBox.className = "film-box";
             container.appendChild(filmBox);
+
+        
         }
+
+        window.addEventListener("load", getData);
+
+        function getData(){
+        const key= "9sJ6NKPiWw3qHmr2sZZwUNmhfDjsWfsP6A9wWfn2";
+        const url = `https://u240066.gluwebsite.nl/api/movies?api_key=${key}`;
+
+        fetch(url)
+        .then((raw) => raw.json()) 
+        .then((data) => {
+        console.log(data.data);
+
+        show(data.data);
+    });
+}
+
+        function show(data) {
+        const filmBoxes = document.querySelectorAll(".film-box");
+        
+        for(let i = 0; i < filmBoxes.length && i < data.length; i++){
+        
+        const box = filmBoxes[i];
+        
+        const title = data[i].movie.title;
+        const poster = data[i].movie.poster_path;
+        const overview = data[i].movie.overview;
+        const vote = data[i].movie.vote_average;
+        const release = data[i].movie.release_date;
+
+        
+        //foto
+        const image= document.createElement("IMG");
+        image.src = `https://image.tmdb.org/t/p/w500/${poster}`;
+        // image.width= 100;
+        image.alt = title;
+        image.id = "poster-image";
+        box.appendChild(image);
+        
+         //titel
+        const titel= document.createElement("h1");
+        titel.textContent = title;
+        box.appendChild(titel);
+
+
+         //release
+        const date= document.createElement("p");
+        date.textContent = release;
+        box.appendChild(date);
+        
+        //overview
+        const over= document.createElement("p");
+        over.textContent = overview;
+        box.appendChild(over);
+
+        // const infoButton = document.createElement("info-knop");
+        //     infoButton.className = "info-knop";
+        //     infoButton.textContent = "MEER INFO & TICKETS";
+            
+        // box.appendChild(infoButton);
+
+        }
+
+};
         </script>
       </div>      
         </div>
--- includes/filmagenda.php
+++ includes/filmagenda.php
@@ -35,14 +35,12 @@
         <select class="categorie-box">
             <h2>
             <option>CATEGORIE</option>
-            <option>ACTIE</option>
-            <option>AVONTUUR</option>
-            <option>KOMEDIE</option>
-            <option>FANTASIE</option>
-            <option>DRAMA</option>
-            <option>HORROR</option>
-            <option>ROMANTIEK</option>
-            <option>THRILLER</option>
+            <option>ACTION</option>
+            <option>ADVENTURE</option>
+            <option>ANIMATION</option>
+            <option>COMEDY</option>
+            <option>CRIME</option>
+            <option>DOCUMENTARY</option>
 </h2>
         </select>
 </label>
@@ -55,16 +53,81 @@
 </div>
   
 <div id="filmsContainer"></div>
-  <div id="film-box">
-    <div id="info-knop"></div>
-  </div>
+  
     <script>
+        
         const container = document.getElementById("filmsContainer");
         for (let i = 0; i < 12; i++) {
             const filmBox = document.createElement("div");
             filmBox.className = "film-box";
             container.appendChild(filmBox);
+
+        
         }
+
+        window.addEventListener("load", getData);
+
+        function getData(){
+        const key= "9sJ6NKPiWw3qHmr2sZZwUNmhfDjsWfsP6A9wWfn2";
+        const url = `https://u240066.gluwebsite.nl/api/movies?api_key=${key}`;
+
+        fetch(url)
+        .then((raw) => raw.json()) 
+        .then((data) => {
+        console.log(data.data);
+
+        show(data.data);
+    });
+}
+
+        function show(data) {
+        const filmBoxes = document.querySelectorAll(".film-box");
+        
+        for(let i = 0; i < filmBoxes.length && i < data.length; i++){
+        
+        const box = filmBoxes[i];
+        
+        const title = data[i].movie.title;
+        const poster = data[i].movie.poster_path;
+        const overview = data[i].movie.overview;
+        const vote = data[i].movie.vote_average;
+        const release = data[i].movie.release_date;
+
+        
+        //foto
+        const image= document.createElement("IMG");
+        image.src = `https://image.tmdb.org/t/p/w500/${poster}`;
+        // image.width= 100;
+        image.alt = title;
+        image.id = "poster-image";
+        box.appendChild(image);
+        
+         //titel
+        const titel= document.createElement("h1");
+        titel.textContent = title;
+        box.appendChild(titel);
+
+
+         //release
+        const date= document.createElement("p");
+        date.textContent = release;
+        box.appendChild(date);
+        
+        //overview
+        const over= document.createElement("p");
+        over.textContent = overview;
+        box.appendChild(over);
+
+        // const infoButton = document.createElement("div");
+        //     infoButton.className = "info-knop";
+        //     infoButton.textContent = "MEER INFO & TICKETS";
+
+        // box.appendChild(infoButton);
+
+        }
+
+};
+
         </script>
       </div>      
         </div>
